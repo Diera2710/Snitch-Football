65f0eaad97841bd1e1cbb75b4c6841a0ee91646c
Diera2710 <dibrienna.rauseuky@ui.ac.id>
2025-09-09 20:57:55 +0700

    deploy

---
+++ main/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+import uuid
+from django.db import models
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('bola', 'Bola'),
+        ('sepatu', 'Sepatu'),
+        ('jersey', 'Jersey'),
+        ('kaos', 'Kaos'),
+        ('celana', 'Celana'),
+    ]
+
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.URLField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='bola')
+    is_featured = models.BooleanField(default=False)
+    
+    
